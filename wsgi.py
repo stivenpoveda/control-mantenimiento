84b88d53e56bd00d83ffed7c8afecd902d6cbc05
chenaoh <cdhenao@gmail.com>
2024-11-29 11:31:30 -0500

    statust

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Ruta a tu proyecto en PythonAnywhere
-project_home = '/home/kevinmaster/project'  # Reemplaza <tu_usuario> por tu nombre de usuario en PythonAnywhere
+project_home = '/home/kevinmaster/'  # Reemplaza <tu_usuario> por tu nombre de usuario en PythonAnywhere
 
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
